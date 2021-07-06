3740b323a09034e37c8114b3a0b94c41a5b21da5
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-06 22:46:08 +0300

    added App component

--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
-import PropTypes from 'prop-types'; // ES6
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const Title = ({ text, id }) => {
+const App = () => {
     return (
         <h1>
-            Hello, {text}, {id}
+            Hello, App.js
         </h1>
-    )
-}
-
-Title.propTypes = {
-    text: PropTypes.string,
-    id: PropTypes.number.isRequired,
-}
-
-Title.defaultProps = {
-    text: "World",
-}
-
-const List = () => {
-    return (
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
     )
 }
 
 const el = (
     <>
-        <Title text="Jack" id={1} />
-        <Title id={2} />
-        <List />
+        <App />
     </>
 )
 

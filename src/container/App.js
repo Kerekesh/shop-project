06d31dfb03a952f22936e0c6c71d148d8010c90d
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-07 20:15:31 +0300

    import Header in App.js

--- src/container/App.js
+++ src/container/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Header from './Header/Header';
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Header from './Header/Header'
 
 const App = () => {
     return (
         <>
             <CssBaseline />
             <Header />
-            <h1>
-                Hello, App.js
-            </h1>
         </>
     )
 }
 
-export default App;
+export default App

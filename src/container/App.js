6e48e30bc0373e1aa68054cef5f5eb35a465d3e8
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-07 20:05:02 +0300

    added component and file Header

--- src/container/App.js
+++ src/container/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from './Header/Header';
 
 const App = () => {
     return (
         <>
             <CssBaseline />
+            <Header />
             <h1>
                 Hello, App.js
             </h1>
-
         </>
     )
 }

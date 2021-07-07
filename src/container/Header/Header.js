18fe5a8fe84101d3af8804b466ec7bf19d328f90
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-07 20:26:54 +0300

    added container in Header

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,7 +1,12 @@
+import Container from '@material-ui/core/Container'
 import React from 'react'
 
 const Header = () => {
-    return <h1>Header</h1>
+    return (
+        <Container maxWidth="lg">
+            <h1>Header</h1>
+        </Container>
+    )
 }
 
 export default Header

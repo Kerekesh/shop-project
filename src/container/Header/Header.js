869f3dd382299230b4f62316ad302f4725940e7a
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-07 20:51:30 +0300

    added AppBar in Header

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,11 +1,45 @@
 import Container from '@material-ui/core/Container'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 import React from 'react'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+    menuButton: {
+        marginRight: 5,
+    },
+    title: {
+        flexGrow: 1,
+    },
+}))
 
 const Header = () => {
+
+    const classes = useStyles()
+
     return (
-        <Container maxWidth="lg">
-            <h1>Header</h1>
-        </Container>
+        <AppBar position="static">
+            <Container>
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Fake shop
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 

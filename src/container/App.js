64d1064f0005e2547edac64760a1fc689161fcaf
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-07 19:55:09 +0300

    added css-baseline

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
     return (
         <>
+            <CssBaseline />
             <h1>
                 Hello, App.js
             </h1>
+
         </>
     )
 }

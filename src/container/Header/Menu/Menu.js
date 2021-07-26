493c6bef31934269ebb34a91550ff414b1ac9545
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-07-26 20:56:36 +0300

    added Routing and link to Cart

--- src/container/Header/Menu/Menu.js
+++ src/container/Header/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
 
 const Menu = () => {
@@ -9,6 +10,9 @@ const Menu = () => {
             <Button color="inherit">Products</Button>
             <Button color="inherit">Payment</Button>
             <Button color="inherit">Shipping</Button>
+            <Button color="inherit">
+                <Link to="/cart">Cart</Link>
+            </Button>
         </>
     )
 }
--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,11 +1,22 @@
 import Container from '@material-ui/core/Container'
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import ProductsList from '../../components/Products/ProductsList'
+import CartPage from '../../pages/CartPage/CartPage'
 
 const Main = ({ addProductToCart }) => {
     return (
         <Container>
-            <ProductsList addProductToCart={addProductToCart} />
+            <Switch>
+                <Route
+                    path="/"
+                    exact
+                    render={() => {
+                        <ProductsList addProductToCart={addProductToCart} />
+                    }} />
+
+                <Route path="/cart" component={CartPage} />
+            </Switch>
         </Container>
     )
 }

14c300435fdacaf1a5b6bdd561767769233d360f
Stepan <30976192+Kerekesh@users.noreply.github.com>
2021-08-09 20:57:11 +0300

    added productsLikeReducer

--- src/redux/productsLikeReducer.js
+++ src/redux/productsLikeReducer.js
@@ -1,5 +1,20 @@
 /* eslint-disable no-unused-vars */
 
-const productsLikeReducer = (state = { 1: true, 2: true }, action) => state
+const productsLikeReducer = (state = { 1: true, 2: true }, action) => {
+    switch (action.type) {
+        case "LIKE":
+            return {
+                ...state,
+                [action.id]: true,
+            }
+        case "DISLIKE":
+            return {
+                ...state,
+                [action.id]: false,
+            }
+        default:
+            return state;
+    }
+}
 
 export default productsLikeReducer
